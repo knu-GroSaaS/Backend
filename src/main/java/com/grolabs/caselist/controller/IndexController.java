8fb37a22fe3f8f4d2798b22f2317681f9276e3e7
Jieunbakk <jieun134@knu.ac.kr>
2024-10-10 23:03:41 +0900

    feat: join 기능 추가

--- src/main/java/com/grolabs/caselist/controller/IndexController.java
+++ src/main/java/com/grolabs/caselist/controller/IndexController.java
@@ -6,9 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class IndexController {
@@ -19,7 +17,8 @@ public class IndexController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @GetMapping("/index")
+
+    @GetMapping("/")
     public String index() {
         return "index";
     }
@@ -30,6 +29,18 @@ public class IndexController {
             return "redirect:/";
         }//login 되어있을시 "/"로 redirect
         return "loginForm";
+    }
+
+
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println(user);
+        user.setUsertype("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String enPassword=passwordEncoder.encode(rawPassword);
+        user.setPassword(enPassword);
+        userRepository.save(user); //회원가입 잘됨 .but 비밀번호: 1234=> 시큐리티로 로그인을 할 수 없음. 이유는 패스워드가 암호화가 안되었기 때문
+        return "redirect:/loginForm";
     }
 
     @GetMapping("/joinForm")
