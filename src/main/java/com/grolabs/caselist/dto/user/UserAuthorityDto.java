108b92e33af865f2109aa63eef328c7922c1bfb8
한재준 <gkswowns25@naver.com>
2024-11-03 00:46:43 +0900

    유저 권한

--- src/main/java/com/grolabs/caselist/dto/user/UserAuthorityDto.java
+++ src/main/java/com/grolabs/caselist/dto/user/UserAuthorityDto.java
@@ -1,2 +1,16 @@
-package com.grolabs.caselist.dto.user;public class UserAuthorityDto {
+package com.grolabs.caselist.dto.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAuthorityDto {
+    private String managerName;
+    private String userName;
+    private String userType;
+
 }
