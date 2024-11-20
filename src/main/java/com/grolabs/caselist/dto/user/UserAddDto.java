dec71eab90b3531397f3d4650a90e038a8264db8
Jieunbakk <jieun134@knu.ac.kr>
2024-11-21 03:25:30 +0900

    feat: auth-login-3 사용자 추가 로직 구현

---
+++ src/main/java/com/grolabs/caselist/dto/user/UserAddDto.java
@@ -0,0 +1,15 @@
+package com.grolabs.caselist.dto.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAddDto {
+    public String requestername;
+    public String username;
+    public String creation;
+}
