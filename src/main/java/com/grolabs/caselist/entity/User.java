1fca24e3a10289db6067d01cf6e859abe0a6d3fb
Jieunbakk <jieun134@knu.ac.kr>
2024-11-21 20:47:56 +0900

    modify: UserEntity SetDeleteTime() 수정

--- src/main/java/com/grolabs/caselist/entity/User.java
+++ src/main/java/com/grolabs/caselist/entity/User.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
+import java.time.Instant;
 
 @Entity
 @Data
@@ -59,6 +60,11 @@ public class User {
         this.emailVerified = true;
         this.emailVerificationToken = null; // 검증 후 토큰 제거
     }
+
+    public void setDeleteTime() {
+        this.deleteTime = Timestamp.from(Instant.now());
+    }
+
 
     public void updateType(String Type){
         this.usertype = Type;
