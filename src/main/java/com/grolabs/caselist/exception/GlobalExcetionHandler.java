fb666c623afc6fc573ad5fc96462485eaad1399f
한재준 <gkswowns25@naver.com>
2024-11-22 09:33:41 +0900

    feat: NotFound핸들러 추가

---
+++ src/main/java/com/grolabs/caselist/exception/GlobalExcetionHandler.java
@@ -0,0 +1,19 @@
+package com.grolabs.caselist.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExcetionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND) // 404 상태 코드
+                .body(ex.getMessage());       // 예외 메시지 반환
+    }
+}
